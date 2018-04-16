9f7c2d521974e0bf718bb279288c7ace0818aeeb
Fryzeec <rward98@outlook.com>
2018-04-16 12:06:33 +1000

    Scroll Script and Prefab

---
+++ Cave_Climber/Assets/Scripts/TextureOffset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffset : MonoBehaviour {
+
+    public float ScrollSpeed = 0.0f;
+    private float AcumulatedTime = 0.0f;
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        AcumulatedTime += Time.deltaTime * ScrollSpeed;
+        float offset = AcumulatedTime;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, offset);
+
+    }
+}
