c2f6f3f5aa906258fa141d2ae384c30d03d4f0c0
Jeremy Little <jezzalittle20@gmail.com>
2018-04-16 11:59:16 +1000

    making a scene for myself

---
+++ Cave_Climber/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    private float gameSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
